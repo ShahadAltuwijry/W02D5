954d3039dc5bad2dcdfe99373df8c083d647983f
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-10-22 00:41:21 +0300

    updated jq task

--- jqToDoLi.js
+++ jqToDoLi.js
@@ -3,14 +3,15 @@
 let i = 0;
 let taskArr = [
   { task: "rest", finished: false },
-  { task: "sleep", finished: true },
+  { task: "sleep", finished: false },
+  { task: "code", finished: false },
 ];
 
 const ren = () => {
   $("#mainList").html("");
   for (let i = 0; i < taskArr.length; i++) {
     $("#mainList").append(
-      `<li> ${taskArr[i].task} <button id = "delTask">Delete</button> <button id = "editTask">edit</button></li>`
+      `<li> ${taskArr[i].task} <button id = "delTask" onclick = "deleteTask(${i})">Delete</button> <button id = "updateTask" onclick = "updateTask(${i})">Update</button></li>`
     );
   }
 };
@@ -25,4 +26,30 @@ const addingTask = () => {
 
 $("#addBtn").click(addingTask);
 
+// deleting button
+const deleteTask = (tasksIndex) => {
+  taskArr = taskArr.filter((task, i) => {
+    if (taskIndex !== index) {
+      return task;
+    }
+  });
+  ren();
+};
+
+//update
+const updateTask = (tasksIndex) => {
+  taskArr.forEach((task, i) => {
+    if (taskIndex === i) {
+      $();
+    }
+  });
+};
+
 // $("#clearList").click();
+
+const tasksCounter = () => {
+  let countArr = taskArr.filter((index) => {
+    return index.finished == false;
+  });
+  $("#taskCounter").text(countArr.length);
+};
